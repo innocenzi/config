a3e3ceb4b9ed8daebf7eff6e1486825e91760d9f
Enzo Innocenzi <enzo@innocenzi.dev>
2020-06-14 19:29:18 +0200

    chore: update for types

--- src/preset.js
+++ src/preset.js
@@ -1,8 +1,11 @@
-module.exports = {
+const { Preset } = require('@use-preset/preset');
+
+module.exports = Preset.make({
+	name: 'dotfiles',
 	actions: () => [
 		{
 			type: 'copy',
 			strategy: 'ask',
 		},
 	],
-};
+});
